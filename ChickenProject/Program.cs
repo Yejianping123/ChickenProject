4f4c48f6c6aea1b4b13f2bb08195d19ed9b49f43
jesse <jesse9726@gmail.com>
2017-05-31 12:42:52 -0700

    Edited Encoder and Decoder, created PricingModel and started on OrderProcessing

--- ChickenProject/Program.cs
+++ ChickenProject/Program.cs
@@ -3,31 +3,53 @@ using System.Threading;
 
 namespace ChickenProject
 {
-    public delegate void priceCutEvent(Int32 pr);
-
+    public delegate void priceCutEvent(double pr);
+    public delegate void orderCompletedEvent();
     public class ChickenFarm
     {
         static Random rng = new Random();
         public static event priceCutEvent priceCut;
-        private static Int32 chickenPrice = 10;
-
-        public Int32 getPrice()
+        private static double chickenPrice = 10;
+        private int p = 0;
+        public double getPrice()
         {
             return chickenPrice;
         }
 
-        public static void changePrice(Int32 price)
+        public static void changePrice(double price)
         {
             if (price < chickenPrice)
             {
                 if (priceCut != null)
                 {
                     priceCut(price);
+
                 }
             }
             chickenPrice = price;
         }
 
+        public void PricingModel()
+        {
+            while (p < 10)
+            {
+                double price = 0;
+                Decimal fff = rng.Next(1,60);
+                Decimal what = fff / 100;
+                double model = (double)what;
+                price = getPrice();
+                if(fff%2 == 0)
+                {
+                    price = price + model;
+                }
+                else
+                {
+                    price = price - model;
+                    p++;
+                }
+                changePrice(price);
+            }
+        }
         public void farmerFunc()
         {
             for (Int32 i = 0; i < 50; i++)
@@ -42,73 +64,73 @@ namespace ChickenProject
         }
     }
 
-        public class Retailer
+    public class Retailer
+    {
+        public void retailerFunc()
         {
-            public void retailerFunc()
+            ChickenFarm chicken = new ChickenFarm();
+            for (Int32 i = 0; i < 10; i++)
             {
-                ChickenFarm chicken = new ChickenFarm();
-                for (Int32 i = 0; i < 10; i++)
-                {
-                    Thread.Sleep(1000);
-                    Int32 p = chicken.getPrice();
-                    Console.WriteLine("Store {0} has everyday low price: ${1} each", Thread.CurrentThread.Name, p);
-                }
-            }
-
-            public void chickenOnSale(Int32 p)
-            {
-                //order chickens from chicken farm - send order into queue
-                Console.WriteLine("Store {0} chickens are on sale: as low as ${1} each", Thread.CurrentThread.Name, p);
+                Thread.Sleep(1000);
+                double p = chicken.getPrice();
+                Console.WriteLine("Store{0} has everyday low price: ${1} each", Thread.CurrentThread.ManagedThreadId, p);
             }
         }
 
-        public class OrderClass
+        public void chickenOnSale(Int32 p)
         {
-            private String senderId;
-            private int cardNo;
-            private int amount;
+            //order chickens from chicken farm - send order into queue
+            Console.WriteLine("Store{0} chickens are on sale: as low as ${1} each", Thread.CurrentThread.ManagedThreadId, p);
+        }
+    }
 
-            public String SenderId
-            {
-                get
-                {
-                    return senderId;
-                }
-                set
-                {
-                    senderId = value;
-                }
-            }
+    public class OrderClass
+    {
+        private String senderId;
+        private int cardNo;
+        private int amount;
 
-            public int CardNo
+        public String SenderId
+        {
+            get
             {
-                get
-                {
-                    return cardNo;
-                }
-                set
-                {
-                    cardNo = value;
-                }
+                return senderId;
             }
+            set
+            {
+                senderId = value;
+            }
+        }
 
-            public int Amount
+        public int CardNo
+        {
+            get
             {
-                get
-                {
-                    return amount;
-                }
-                set
-                {
-                    amount = value;
-                }
+                return cardNo;
             }
+            set
+            {
+                cardNo = value;
+            }
+        }
+
+        public int Amount
+        {
+            get
+            {
+                return amount;
+            }
+            set
+            {
+                amount = value;
+            }
+        }
 
         public void toString()
         {
-            Console.WriteLine("Name: "+senderId+"\nCard: "+cardNo+"\nAmount: "+amount);
+            Console.WriteLine("Name: " + senderId + "\nCard: " + cardNo + "\nAmount: " + amount);
         }
-        }
+    }
 
     public class Encoder
     {
@@ -136,29 +158,61 @@ namespace ChickenProject
         }
     }
 
-        public class myApplication
+    public class OrderProcessing
+    {
+        public static void confirmOrder(OrderClass orderObject, int unitPrice)
         {
-            static void Main(string[] args)
+            double tax = .05;
+            double shippingHandling;
+            if(orderObject.CardNo>=5000 && orderObject.CardNo<=7000)
             {
-            OrderClass order = new OrderClass();
-            order.SenderId = "Jesse";
-            order.CardNo = 41234;
-            order.Amount = 10094;
-            order.toString();
-            String encoded = Encoder.Encode(order);
-            OrderClass decodedOrder = Decoder.Decode(encoded);
-            decodedOrder.toString();
-                /*ChickenFarm chicken = new ChickenFarm();
-                Thread farmer = new Thread(new ThreadStart(chicken.farmerFunc));
-                farmer.Start();
-                Retailer chickenstore = new Retailer();
-                ChickenFarm.priceCut += new priceCutEvent(chickenstore.chickenOnSale);
-                Thread[] retailers = new Thread[3];
-                for (int i = 0; i < 3; i++)
+                int NoOfChickens = orderObject.Amount;
+                double total = 0;
+                total = NoOfChickens * unitPrice;
+                if (NoOfChickens < 50)
                 {
-                    retailers[i] = new Thread(new ThreadStart(chickenstore.retailerFunc));
-                    retailers[i].Start();
-                }*/
+                    shippingHandling = 100;
+                }
+                else
+                {
+                    shippingHandling = 50;
+                }
+                total = total + (total * tax) + shippingHandling;
+
+                Console.WriteLine("Order from {0}\n\tAmount: {1}\n\tCredit Card Number: {2}\n\tChicken Price: {3}\n\tOrder Cost: {4}",
+orderObject.SenderId, orderObject.Amount, orderObject.CardNo, unitPrice, total);
             }
         }
+    }
+
+    public class myApplication
+    {
+        static void Main(string[] args)
+        {
+            OrderClass order = new OrderClass();
+            order.SenderId = "Jesse";
+            order.CardNo = 5155;
+            order.Amount = 10094;
+            String encoded = Encoder.Encode(order);
+            OrderClass decodedOrder = Decoder.Decode(encoded);         
+            OrderProcessing.confirmOrder(decodedOrder, 7);
+            ChickenFarm chicken = new ChickenFarm();
+            while(true)
+            {
+                Thread.Sleep(500);
+                chicken.PricingModel();
+            }
+           /* ChickenFarm chicken = new ChickenFarm();
+            Thread farmer = new Thread(new ThreadStart(chicken.farmerFunc));
+            farmer.Start();
+            Retailer chickenstore = new Retailer();
+            ChickenFarm.priceCut += new priceCutEvent(chickenstore.chickenOnSale);
+            Thread[] retailers = new Thread[5];
+            for (int i = 0; i < 5; i++)
+            {
+                retailers[i] = new Thread(new ThreadStart(chickenstore.retailerFunc));
+                retailers[i].Start();
+            }*/
+        }
+    }
 }
