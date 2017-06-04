739b271f2ab380ecbc4bd778a27010a90c13178e
jesse <jesse9726@gmail.com>
2017-06-03 17:32:12 -0700

    working on multicellbuffer

--- ChickenProject/Program.cs
+++ ChickenProject/Program.cs
@@ -23,31 +23,41 @@ namespace ChickenProject
                 if (priceCut != null)
                 {
                     priceCut(price);
-
                 }
             }
             chickenPrice = price;
+            String processing = myApplication.buffer.getOneCell();
+            OrderClass orderToProcess = Decoder.Decode(processing);
+            OrderProcessing process = new OrderProcessing(orderToProcess, price);
+            
         }
 
         public void PricingModel()
         {
             while (p < 10)
             {
+                Thread.Sleep(500);
                 double price = 0;
                 Decimal fff = rng.Next(1,60);
                 Decimal what = fff / 100;
                 double model = (double)what;
                 price = getPrice();
+                //Console.WriteLine("Old Price: "+price);
                 if(fff%2 == 0)
                 {
+                   // Console.WriteLine("Price increased to: " + price+"\n");
                     price = price + model;
                 }
                 else
                 {
+                    
                     price = price - model;
                     p++;
+                   // Console.WriteLine("Price dropped to: " + price + "\nThis is price drop #: " + p + "\n");
                 }
+                Console.WriteLine("New Price: " + price);
                 changePrice(price);
+
             }
         }
         public void farmerFunc()
@@ -62,25 +72,42 @@ namespace ChickenProject
                 ChickenFarm.changePrice(p);
             }
         }
+
+
+
     }
 
     public class Retailer
     {
+       // public ChickenFarm chicken = new ChickenFarm();
+        public double price;
+        public bool needorder = false;
+        public TimeSpan orderSent;
         public void retailerFunc()
         {
             ChickenFarm chicken = new ChickenFarm();
-            for (Int32 i = 0; i < 10; i++)
+            price = chicken.getPrice();
+            Console.WriteLine("Store{0} initial chicken Price: {1}", Thread.CurrentThread.Name,price);
+            while(true)
             {
-                Thread.Sleep(1000);
-                double p = chicken.getPrice();
-                Console.WriteLine("Store{0} has everyday low price: ${1} each", Thread.CurrentThread.ManagedThreadId, p);
-            }
+                if(needorder==true)
+                {
+                    OrderClass order = new OrderClass();
+                    order.SenderId = Thread.CurrentThread.Name;
+                    order.CardNo = 5000 + int.Parse(Thread.CurrentThread.Name);
+                    order.Amount = 25;
+                    String encodedorder = Encoder.Encode(order);
+                    orderSent = DateTime.Now.TimeOfDay;
+                    myApplication.buffer.setOneCell(encodedorder);
+                    needorder = false;
+                }
+            }         
         }
 
-        public void chickenOnSale(Int32 p)
+        public void chickenOnSale(double p)
         {
-            //order chickens from chicken farm - send order into queue
-            Console.WriteLine("Store{0} chickens are on sale: as low as ${1} each", Thread.CurrentThread.ManagedThreadId, p);
+            price = p;
+            needorder = true;
         }
     }
 
@@ -137,7 +164,7 @@ namespace ChickenProject
         public static String Encode(OrderClass orderObject)
         {
             String encoded = orderObject.SenderId + "/" + orderObject.CardNo + "/" + orderObject.Amount;
-            Console.WriteLine("Encoded string: " + encoded);
+           // Console.WriteLine("Encoded Order: " + encoded);
             return encoded;
         }
     }
@@ -160,7 +187,8 @@ namespace ChickenProject
 
     public class OrderProcessing
     {
-        public static void confirmOrder(OrderClass orderObject, int unitPrice)
+        //public static void confirmOrder(OrderClass orderObject, int unitPrice)
+        public OrderProcessing(OrderClass orderObject, double unitPrice)
         {
             double tax = .05;
             double shippingHandling;
@@ -185,25 +213,74 @@ orderObject.SenderId, orderObject.Amount, orderObject.CardNo, unitPrice, total);
         }
     }
 
+
+    public class MultiCellBuffer
+    {
+        int N;
+        String[] cells;
+        int loc = 0;
+        public MultiCellBuffer()
+        {
+            N = 5;
+            cells = new String[5];
+            for(int i=0;i<N;i++)
+            {
+                cells[i] = "";
+            }
+            
+        }
+        public void setOneCell(String encodedOrder)
+        {
+            Console.WriteLine("You got inside setOneCell, LOC= " + loc);
+            cells[loc] = encodedOrder;
+            loc++;
+        }
+
+        public String getOneCell()
+        {
+            loc--;
+            Console.WriteLine("You got inside getonecell, LOC= " + loc);
+            return cells[loc];
+        }
+    }
+
     public class myApplication
     {
+        public static MultiCellBuffer buffer = new MultiCellBuffer();
         static void Main(string[] args)
         {
-            OrderClass order = new OrderClass();
-            order.SenderId = "Jesse";
-            order.CardNo = 5155;
-            order.Amount = 10094;
-            String encoded = Encoder.Encode(order);
-            OrderClass decodedOrder = Decoder.Decode(encoded);         
-            OrderProcessing.confirmOrder(decodedOrder, 7);
+            /* OrderClass order = new OrderClass();
+             order.SenderId = "Jesse";
+             order.CardNo = 5155;
+             order.Amount = 10094;
+             String encoded = Encoder.Encode(order);
+             OrderClass decodedOrder = Decoder.Decode(encoded);         
+             OrderProcessing.confirmOrder(decodedOrder, 7);
+             ChickenFarm chicken = new ChickenFarm();
+             while(true)
+             {
+                 Thread.Sleep(500);
+                 chicken.PricingModel();
+             }
+             */
             ChickenFarm chicken = new ChickenFarm();
-            while(true)
+            Thread farmer = new Thread(new ThreadStart(chicken.PricingModel));
+            farmer.Start();
+            Retailer store = new Retailer();
+            //Thread calc = new Thread(new ThreadStart(store.chicken.PricingModel));
+            //calc.Start();
+            ChickenFarm.priceCut += new priceCutEvent(store.chickenOnSale);
+            Thread[] retailers = new Thread[5];
+            for (int i = 0; i < 5; i++)
             {
-                Thread.Sleep(500);
-                chicken.PricingModel();
+                retailers[i] = new Thread(new ThreadStart(store.retailerFunc));
+                retailers[i].Name = (i + 1).ToString();
+                retailers[i].Start();
             }
-           /* ChickenFarm chicken = new ChickenFarm();
-            Thread farmer = new Thread(new ThreadStart(chicken.farmerFunc));
+
+            /*
+            ChickenFarm chicken = new ChickenFarm();
+            Thread farmer = new Thread(new ThreadStart(chicken.PricingModel));
             farmer.Start();
             Retailer chickenstore = new Retailer();
             ChickenFarm.priceCut += new priceCutEvent(chickenstore.chickenOnSale);
@@ -211,8 +288,25 @@ orderObject.SenderId, orderObject.Amount, orderObject.CardNo, unitPrice, total);
             for (int i = 0; i < 5; i++)
             {
                 retailers[i] = new Thread(new ThreadStart(chickenstore.retailerFunc));
+                retailers[i].Name = (i + 1).ToString();
                 retailers[i].Start();
-            }*/
+                while (!retailers[i].IsAlive) ;
+            }
+            */
+
+            /*
+             ChickenFarm chicken = new ChickenFarm();
+             Thread farmer = new Thread(new ThreadStart(chicken.farmerFunc));
+             farmer.Start();
+             Retailer chickenstore = new Retailer();
+             ChickenFarm.priceCut += new priceCutEvent(chickenstore.chickenOnSale);
+             Thread[] retailers = new Thread[5];
+             for (int i = 0; i < 5; i++)
+             {
+                 retailers[i] = new Thread(new ThreadStart(chickenstore.retailerFunc));
+                 retailers[i].Start();
+             }
+             */
         }
     }
 }
